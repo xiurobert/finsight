5496ac5b7859d80b519f39fad7070750004c8136
Robert Xiu <xiurobert@gmail.com>
2022-04-25 18:47:43 +0800

    Cleaned up code and moved network into model file

--- src/datasets/huge_stock_ds/spy.py
+++ src/datasets/huge_stock_ds/spy.py
@@ -5,17 +5,20 @@ import pandas as pd
 
 
 class SPYDataset(data.Dataset):
-    def __init__(self, data_dir: str, features: list[str], label: str):
+    def __init__(self, data_dir: str, features: list[str], label: str, dtype=torch.float32):
         """
         Constructs a SPYDataset. If hidden columns are specified, they will be dropped from the dataframe
 
         :param data_dir: The data directory. This should contain the folders Stocks and ETFs
         :param features: The feature columns to be used
         :param label: The label column to be used
+        :param dtype: The dtype of the tensors (Currently does not do anything)
+        :type dtype: torch.dtype
         """
         self.data_dir = data_dir
         self.features = features
         self.label = label
+        self.dtype = dtype
         self._file_path = os.path.join(self.data_dir, "ETFs", "spy.us.txt")
         self.data = pd.read_csv(self._file_path)
         self.data['Date'] = pd.to_datetime(self.data['Date'])
@@ -24,7 +27,12 @@ class SPYDataset(data.Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        # TODO: Fix this soon
         features = self.data.iloc[idx][self.features].values
         label = self.data.iloc[idx][[self.label]].values
-        return torch.Tensor(list(features)), label[0]
+        features = torch.Tensor(list(features))
+        label = torch.Tensor(list(label))
+        if self.dtype:
+            label = label.to(self.dtype)
+            features = features.to(self.dtype)
+
+        return features, label
--- src/models/basic_dnn_regression.py
+++ src/models/basic_dnn_regression.py
@@ -1,7 +1,45 @@
 import torch
 import torch.nn as nn
+import torch.utils.data
+from tqdm import tqdm
 
 
 class BasicDNNRegression(nn.Module):
     def __init__(self):
         super(BasicDNNRegression, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Linear(1, 32),
+            nn.ReLU(),
+            nn.Linear(32, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+
+    def forward(self, x):
+        x = self.layer1(x)
+        return x
+
+    def train_one_epoch(self,
+                        train_loader: torch.utils.data.DataLoader,
+                        optimizer: torch.optim.Optimizer,
+                        criterion: torch.nn.Module,
+                        epoch_number=0):
+        dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.to(dev)
+        self.train()
+        loss_tally = 0
+        for i, (features, labels) in enumerate((prog_bar := tqdm(train_loader, total=len(train_loader)))):
+            features, labels = features.to(dev), labels.to(dev)
+            # labels = labels.view(-1, 1)
+
+            predictions = self(features)
+            loss = criterion(predictions, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            loss_tally += loss.item()
+            prog_bar.set_description(f"Epoch: {epoch_number + 1}, "
+                                     f"Batch #: {i+1}, "
+                                     f"Current Loss: {loss.item():.3f}, "
+                                     f"Avg Loss: {loss_tally / (i + 1):.3f}")
